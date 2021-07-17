21b048839ccf8ee01feb1672b60405f6eb1da6bf
Rafalhel <rafaelgs04@gmail.com>
2021-07-17 19:19:24 -0300

    Finalizado

--- Multiplicador.py
+++ Multiplicador.py
@@ -1,9 +1,68 @@
-def multplicador():
-    try:
-        mult=int(input(f"Digite um número para ver seus múltiplos de 1 a 10:\n"))
-        for i in range (1,11):
-            print(f'{mult} * {i} = {mult*i}')
-    except ValueError:
-        return f'Valor inválido'
+class Operacoes:
+    def __init__(self):
+        try:
+            self.escolha = int(input(f'Qual operação deseja usar ?\n'
+                                f'1- Soma\n2- subtração\n3- Multiplicação\n'
+                                f'4- Divisão\n'))
+        except:
+            print('Valor inválido')
+            return
+        if self.escolha == 1:
+            self.soma()
+        elif self.escolha == 2:
+            self.sub()
+        elif self.escolha == 3:
+            self.multplicador()
+        elif self.escolha == 4:
+            self.divisor()
+        else:
+            print('Operação inválida')
+            return
 
-multplicador()
+    def multplicador(self):
+            try:
+                self.mult=int(input(f"Digite um número para ver seus múltiplos de 1 a 10:\n"))
+                for i in range (1,11):
+                    print(f'{self.mult} * {i} = {self.mult*i}')
+            except ValueError:
+                print(f'Valor inválido')
+
+    def divisor(self):
+        try:
+            self.mult=int(input(f"Digite um número para ver seus divisores de 1 a 10:\n"))
+            for i in range (1,11):
+                print(f'{self.mult} / {i} = {self.mult/i}')
+        except ValueError:
+            print(f'Valor inválido')
+        except ZeroDivisionError:
+            print(f'Não é possível dividir por 0')
+
+    def soma(self):
+        try:
+            self.mult=int(input(f"Digite um número para ver suas somas de 1 a 10:\n"))
+            for i in range (1,11):
+                print(f'{self.mult} + {i} = {self.mult+i}')
+        except ValueError:
+            print(f'Valor inválido')
+
+    def sub(self):
+        try:
+            self.mult = int(input(f"Digite um número para ver suas subtrações de 1 a 10:\n"))
+            for i in range(1, 11):
+                print(f'{self.mult} - {i} = {self.mult - i}')
+        except ValueError:
+            print(f'Valor inválido')
+
+
+if __name__ == '__main__':
+
+    a=True
+    while a:
+        x = Operacoes()
+        t = input('Deseja continuar ? (S/N) ')
+        t = t.lower()
+        if t == 's' or t == 'y':
+            a = True
+        elif t == 'n':
+            print(f'Até a próxima')
+            a = False
