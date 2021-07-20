efa5028193021476adee489faf85b050c25ee5ad
Rafalhel <rafaelgs04@gmail.com>
2021-07-19 23:46:04 -0300

    Finalizado

--- Multiplicador.py
+++ Multiplicador.py
@@ -54,15 +54,3 @@ class Operacoes:
             print(f'Valor inválido')
 
 
-if __name__ == '__main__':
-
-    a=True
-    while a:
-        x = Operacoes()
-        t = input('Deseja continuar ? (S/N) ')
-        t = t.lower()
-        if t == 's' or t == 'y':
-            a = True
-        elif t == 'n':
-            print(f'Até a próxima')
-            a = False
