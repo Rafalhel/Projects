b40237df3b9b85758c4875bc4bd1e97a7a260417
Rafalhel <rafaelgs04@gmail.com>
2021-07-19 23:46:42 -0300

    Finalizado

---
+++ main.py
@@ -0,0 +1,17 @@
+from Multiplicador import Operacoes
+
+if __name__ == '__main__':
+
+    a=True
+    while a:
+        x = Operacoes()
+        t = input('Deseja continuar ? (S/N) ')
+        t = t.lower()
+        if t == 's' or t == 'y':
+            a = True
+        elif t == 'n':
+            print(f'Até a próxima')
+            a = False
+        else:
+            print(f'Valor inválido')
+            a=False
