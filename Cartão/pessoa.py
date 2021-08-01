bcdeef8c652b0f89ecf69a8723672eadbd222869
Rafalhel <rafaelgs04@gmail.com>
2021-07-31 23:50:45 -0300

    Exclui parcelas pagas totalmente e mantem as que ainda não foram

--- Cartão/pessoa.py
+++ Cartão/pessoa.py
@@ -6,6 +6,25 @@ class Pessoa:
     def __init__(self, nome=input("Digite seu nome: ")):
         self.nome = nome
         self.item = []
+        self.dia = int(datetime.today().strftime('%d'))
+        self.mes = int(datetime.today().strftime('%m'))
+        self.ano = int(datetime.today().strftime('%Y'))
+        dataCartao = 5
+        cp = []
+        print(self.dia)
+        if self.dia == dataCartao:
+            with open(f'{self.nome}.csv', 'r') as f:
+                reader = csv.reader(f)
+                for i in reader:
+                    x = int(i[3].replace('/',''))
+                    hoje = str(self.mes)+str(self.ano)
+                    if x >= int(hoje):
+                        cp.append(i)
+            if cp != '':
+                with open(f'{self.nome}.csv', 'w') as f:
+                    writer = csv.writer(f, lineterminator='\r')
+                    for i in cp:
+                        writer.writerow(i)
 
         try:
             with open(f'{self.nome}.csv', 'r') as f:
@@ -28,16 +47,15 @@ class Pessoa:
         vezes = int(input("Parcelas: "))
         if vezes == '':
             return 'Valor inválido'
-        mes = int(datetime.today().strftime('%m'))
-        ano = int(datetime.today().strftime('%Y'))
-        meses = mes
+
+        meses = self.mes
         for i in range(vezes):
             meses += 1
             if meses == 12:
                 meses = 0
-                ano ==1
+                self.ano ==1
 
-        mesfinal = str(meses)+'/'+str(ano)
+        mesfinal = str(meses)+'/'+str(self.ano)
         # final = int(mesfinal.replace('/',''))
         # print(final)
         print(mesfinal)
@@ -47,7 +65,7 @@ class Pessoa:
         print(f'{item} - R${parcela}')
         with open(f'{self.nome}.csv', 'a') as f:
             writer = csv.writer(f, lineterminator='\r')
-            writer.writerow([item, parcela, mes,mesfinal])
+            writer.writerow([item, parcela, self.mes,mesfinal])
 
     def lista(self):
         with open(f'{self.nome}.csv', 'r') as f:
