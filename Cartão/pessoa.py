808d164aad01d7956b3f3f1f8e44ec57945fb970
Rafalhel <rafaelgs04@gmail.com>
2021-10-15 23:03:29 -0300

    Mudei algo

--- Cartão/pessoa.py
+++ Cartão/pessoa.py
@@ -9,11 +9,11 @@ class Pessoa:
         self.dia = int(datetime.today().strftime('%d'))
         self.mes = int(datetime.today().strftime('%m'))
         self.ano = int(datetime.today().strftime('%Y'))
-        dataCartao = 5
+        dataCartao = 10
         cp = []
-        print(self.dia)
+        # print(self.dia)
         if self.dia == dataCartao:
-            with open(f'{self.nome}.csv', 'r') as f:
+            with open(f'dados/{self.nome}.csv', 'r') as f:
                 reader = csv.reader(f)
                 for i in reader:
                     x = int(i[3].replace('/',''))
@@ -21,18 +21,18 @@ class Pessoa:
                     if x >= int(hoje):
                         cp.append(i)
             if cp != '':
-                with open(f'{self.nome}.csv', 'w') as f:
+                with open(f'dados/{self.nome}.csv', 'w') as f:
                     writer = csv.writer(f, lineterminator='\r')
                     for i in cp:
                         writer.writerow(i)
 
         try:
-            with open(f'{self.nome}.csv', 'r') as f:
+            with open(f'dados/{self.nome}.csv', 'r') as f:
                 reader = csv.reader(f)
                 for i in reader:
                     self.item.append(i)
         except FileNotFoundError:
-            with open(f'{self.nome}.csv', 'w') as f:
+            with open(f'dados/{self.nome}.csv', 'w') as f:
                 reader = csv.writer(f)
 
     def add(self):
@@ -47,35 +47,49 @@ class Pessoa:
         vezes = int(input("Parcelas: "))
         if vezes == '':
             return 'Valor inválido'
+        self.mes = int(input("Mes: "))
+        if self.mes == '':
+            return 'Valor inválido'
+        self.ano = int(input("Ano: "))
+        if self.ano == '':
+            return 'Valor inválido'
+
+        mesAtual = f'{str(self.mes)}/{str(self.ano)}'
 
         meses = self.mes
         for i in range(vezes):
             meses += 1
-            if meses == 12:
-                meses = 0
-                self.ano ==1
+            if meses > 12:
+                meses = 1
+                self.ano +=1
 
         mesfinal = str(meses)+'/'+str(self.ano)
         # final = int(mesfinal.replace('/',''))
         # print(final)
+
+        print(mesAtual)
         print(mesfinal)
 
         parcela = total / vezes
+        parcela = round(parcela,2)
 
         print(f'{item} - R${parcela}')
-        with open(f'{self.nome}.csv', 'a') as f:
+        with open(f'dados/{self.nome}.csv', 'a') as f:
             writer = csv.writer(f, lineterminator='\r')
-            writer.writerow([item, parcela, self.mes,mesfinal])
+            writer.writerow([item, parcela, mesAtual,mesfinal])
 
     def lista(self):
-        with open(f'{self.nome}.csv', 'r') as f:
+        with open(f'dados/{self.nome}.csv', 'r') as f:
             reader = csv.reader(f)
+            soma = 0
             for i in reader:
                 print(f'{i[0]} - R${i[1]}')
+                soma += float(i[1])
+            print(f'Total R${soma:.2f}')
             return
 
     def total(self):
-        with open(f'{self.nome}.csv', 'r') as f:
+        with open(f'dados/{self.nome}.csv', 'r') as f:
             reader = csv.reader(f)
             soma = 0
             for i in reader:
@@ -84,5 +98,5 @@ class Pessoa:
             return
 
     def excluir(self):
-        with open(f'{self.nome}.csv', 'w') as f:
+        with open(f'dados/{self.nome}.csv', 'w') as f:
             reader = csv.writer(f)
